87ed23fec3e1a1c53b0bc190d2a79f50858212af
= <nathaniel.risum2@gmail.com>
2025-11-13 13:44:45 +0100

    Initial commit

---
+++ Spike_TCP_502/Spike_TCP_502/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Sockets;
+
+
+
+TcpClient tcpClient = new TcpClient("192.168.123.100",501);
+Console.WriteLine("Hello, World!");
+
+tcpClient.Close();
