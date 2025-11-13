e88f26c8e02a5f6940ce419eb9eafb3ee0976f5c
= <nathaniel.risum2@gmail.com>
2025-11-13 14:04:31 +0100

    Add new spike to send commands

---
+++ Spike_TCP_502/SendCommandToModbus/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[StructLayout(LayoutKind.Explicit, Size = 7, CharSet = CharSet.Ansi)]
+struct ModbusHeader {
+    [FieldOffset(0)] UInt16 transactionmodifier;
+    [FieldOffset(2)] UInt16 protocolidentifier;
+    [FieldOffset(4)] UInt16 length;
+    [FieldOffset(6)] byte unitidentifier; 
+}
+
+class ModbusMessage
+{
+    ModbusHeader header;
+    List<byte> data; 
+
+
+
+}
--- Spike_TCP_502/Spike_TCP_502/Program.cs
+++ Spike_TCP_502/Spike_TCP_502/Program.cs
@@ -1,9 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Net.Sockets;
 
+// This we like
+TcpClient tcpClient = new TcpClient("192.168.123.100", 502);
+Console.WriteLine("Hello, World!");
+
+tcpClient.Close();
+tcpClient = null; 
 
 
-TcpClient tcpClient = new TcpClient("192.168.123.100",501);
+// This we do not like
+tcpClient = new TcpClient("192.168.123.100",501);
 Console.WriteLine("Hello, World!");
 
 tcpClient.Close();
