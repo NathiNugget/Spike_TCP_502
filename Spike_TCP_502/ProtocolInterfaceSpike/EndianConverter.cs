94294cc96f2ec002c77e5da87f285e52ac1cdf90
Marius Egede Knudsen <mak004@edu.zealand.dk>
2025-11-14 10:50:34 +0100

    endian convert

---
+++ Spike_TCP_502/ProtocolInterfaceSpike/EndianConverter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtocolInterfaceSpike
+{
+    static public class EndianConverter
+    {
+        static uint FromHostToNetwork(uint val)
+        {
+            if(BitConverter.IsLittleEndian)
+            {
+                BinaryPrimitives.ReverseEndianness(val);
+            }
+            return val;
+        }
+        static int FromHostToNetwork(int val)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                BinaryPrimitives.ReverseEndianness(val);
+            }
+            return val;
+        }
+        static ushort FromHostToNetwork(ushort val)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                BinaryPrimitives.ReverseEndianness(val);
+            }
+            return val;
+        }
+        static short FromHostToNetwork(short val)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                BinaryPrimitives.ReverseEndianness(val);
+            }
+            return val;
+        
+        }
+
+
+
+        static uint FromNetworkToHost(uint val)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                BinaryPrimitives.ReverseEndianness(val);
+            }
+            return val;
+        }
+        static int FromNetworkToHost(int val)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                BinaryPrimitives.ReverseEndianness(val);
+            }
+            return val;
+        }
+        static ushort FromNetworkToHost(ushort val)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                BinaryPrimitives.ReverseEndianness(val);
+            }
+            return val;
+        }
+        static short FromNetworkToHost(short val)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                BinaryPrimitives.ReverseEndianness(val);
+            }
+            return val;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
